0b34a0e836de121968ccb45cf1a447d0f0382bd6
XiaoWuTu <LZ.XWT@outlook.com>
2023-01-18 11:14:22 +0800

    2023.1.18(半成品)

--- main.py
+++ main.py
@@ -76,9 +76,9 @@ def append():
     e2_content = ttkbootstrap.StringVar()
     e2 = ttkbootstrap.Entry(lf, width=300, textvariable=e2_content)
     l5 = ttkbootstrap.Label(lf, text="请输入计划内容：", font=("楷体", 10))
-    l6 = ttkbootstrap.Label(lf, text="请输入计划开始时间(格式：时：分，时分均为两位数（0补齐），冒号为英文状态。):")
+    l6 = ttkbootstrap.Label(lf, text="请输入计划开始时间(格式：时：分，时分均为两位数（0补齐），冒号为英文状态。):", font=("楷体", 10))
     b4 = ttkbootstrap.Button(lf, text="确认添加", command=get_append, width=300)
-    l7 = ttkbootstrap.Label(lf, text="请输入计划截止时间(格式：时：分，时分均为两位数（0补齐），冒号为英文状态。):")
+    l7 = ttkbootstrap.Label(lf, text="请输入计划截止时间(格式：时：分，时分均为两位数（0补齐），冒号为英文状态。):", font=("楷体", 10))
     e3_content = ttkbootstrap.StringVar()
     e3 = ttkbootstrap.Entry(lf, width=300, textvariable=e3_content)
     l2.pack()
@@ -100,7 +100,7 @@ def get_append():
     end_time = str(de2.entry.get()) + " " + str(e3_content.get())
     what = str(e1_content.get())
     r = 0
-    for i in plans:
+    for _ in plans:
         r = r + 1
     plans.append((r + 1, start_time, end_time, what))
     with open("plans.pickle", "wb+") as f:
@@ -115,7 +115,76 @@ def get_append():
 
 def modification():
     cls_append()
+    global l8, l9, e4_content, e4, cbo, b4, r1, r2, r3
+    l8 = ttkbootstrap.Label(lf, text="计划修改页面", font=("楷体", 20))
+    l9 = ttkbootstrap.Label(lf, text="请选择要修改计划的序号", font=("楷体", 10))
+    e4_content = ttkbootstrap.StringVar()
+    e4 = ttkbootstrap.Entry(lf, width=300, textvariable=e4_content)
+    plan = 0
+    plans_number = []
+    for _ in plans:
+        plan = plan + 1
+        plans_number.append(plan)
+    cbo = ttkbootstrap.Combobox(
+        master=lf,
+        font=("仿宋", 10),
+        values=plans_number,
+        width=210
+    )
+    l8.pack()
+    l9.pack()
+    cbo.pack()
 
+    def ensure(event):
+        global cbo_get
+        cbo_get = int(cbo.get()) - 1
+
+    cbo.bind('<<ComboboxSelected>>', ensure)
+    variable_value = ttkbootstrap.StringVar()
+    variable_value_dist = {
+        "1": "修改计划的起始时间",
+        "2": "修改计划的截止时间",
+        "3": "修改计划的内容"
+    }
+    r1 = ttkbootstrap.Radiobutton(lf, text='修改起始时间', variable=variable_value, value=1, width=50)
+    r1.pack(side=ttkbootstrap.LEFT)
+    r2 = ttkbootstrap.Radiobutton(lf, text='修改截止时间', variable=variable_value, value=2, width=50)
+    r2.pack(side=ttkbootstrap.LEFT)
+    r3 = ttkbootstrap.Radiobutton(lf, text='修改计划内容', variable=variable_value, value=3, width=50)
+    r3.pack(side=ttkbootstrap.LEFT)
+
+    def ensure():
+        global l10, l11, l12, de3, l13, e5_content, e5
+        r1.pack_forget()
+        r2.pack_forget()
+        r3.pack_forget()
+        b4.pack_forget()
+        l9.pack_forget()
+        e4.pack_forget()
+        cbo.pack_forget()
+        if variable_value.get() == "1":
+            l10 = ttkbootstrap.Label(lf, text=variable_value_dist[variable_value.get()] + "页面", font=("楷体", 15))
+            l10.pack()
+            l11 = ttkbootstrap.Label(lf, text="原计划起始时间:" + plans[cbo_get][1], font=("楷体", 10))
+            l11.pack()
+            l12 = ttkbootstrap.Label(lf, text="请选择修改后的计划起始日期：", font=("楷体", 10))
+            l12.pack()
+            de3 = ttkbootstrap.DateEntry(lf, width=207)
+            de3.pack()
+            l13 = ttkbootstrap.Label(lf, text="请输入修改后的计划开始时间(格式：时：分，时分均为两位数（0补齐），冒号为英文状态。):", font=("楷体", 10))
+            l13.pack()
+            e5_content = ttkbootstrap.StringVar()
+            e5 = ttkbootstrap.Entry(lf, width=300, textvariable=e5_content)
+            b5 = ttkbootstrap.Button(lf, text="确定修改", command=get_modification, width=20)
+            b5.pack()
+    b4 = ttkbootstrap.Button(lf, text="确定", command=ensure, width=20)
+    b4.pack(side=ttkbootstrap.LEFT)
+
+def get_modification():
+    start_time = str(de3.entry.get()) + " " + str(e5_content.get())
+    plans[cbo_get][1] = start_time
+    with open("plans.pickle", "wb+") as f:
+        pickle.dump(plans, f)
 
 def backspace():
     cls_append()
